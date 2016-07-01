1d64e6b8729384373c98631ee7a8cef43a869aa5
Jayant Karmakar <joy4u2@users.noreply.github.com>
2016-07-01 20:30:50 +0530

    Changes

--- run_analysis.R
+++ run_analysis.R
@@ -121,7 +121,9 @@ library(dplyr);
 
 MasterDAta2 <- data.table(MasterData)
 #This takes the mean of every column broken down by participants and activities
-TidyData <- MasterDAta2[, lapply(.SD, mean), by = 'subject,activity']
-write.table(TidyData, file = "Tidy_Data.txt", row.names = FALSE)
+library(tidyr)
+TidyData <- group_by(MasterData, subject, activity) %>% summarise_all(funs(mean)) %>% gather(measurement, mean, -activity, -subject)
+
+write.table(TidyData, file = "Tidy_DaTa.txt", row.names = FALSE)
 
 ##END
